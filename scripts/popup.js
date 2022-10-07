1d349ba6ce16953f72a96e09ed82c55bf4eddb2e
jorisgrls <jorisgrilleres@gmail.com>
2022-10-07 13:37:59 +0200

    Fix popup

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,4 +1,4 @@
-let facts = ["Le Pokémon le plus puissant est Mewtwo, avec 800 points de statistiques totales.","Le Pokémon le plus petit est Minidraco, avec 0,3 mètre de hauteur.","Le Pokémon le plus grand est Méga-Gyarados, avec 6,5 mètres de hauteur.","Le Pokémon le plus rapide est Deoxys, avec 180 km/h.","Le Pokémon le plus lourd est Méga-Gyarados, avec 305 kg.","Le Pokémon le plus léger est Minidraco, avec 0,3 kg.","Le Pokémon le plus fort en attaque est Mewtwo, avec 300 points d'attaque.","Le Pokémon le plus fort en défense est Méga-Shuckle, avec 230 points de défense.","Le Pokémon le plus fort en attaque spéciale est Mewtwo, avec 300 points d'attaque spéciale.","Le Pokémon le plus fort en défense spéciale est Méga-Shuckle, avec 230 points de défense spéciale.","Le Pokémon le plus fort en vitesse est Deoxys, avec 180 points de vitesse.","Le Pokémon le plus fort en précision est Méga-Shuckle, avec 230 points de précision.","Le Pokémon le plus fort en esquive est Méga-Shuckle, avec 230 points d'esquive.","Le Pokémon le plus fort en chance est Méga-Shuckle, avec 230 points de chance.","Le Pokémon le plus fort en évitement est Méga-Shuckle, avec 230 points d'évitement.","Le Pokémon le plus fort en blocage est Méga-Shuckle, avec 230 points de blocage.","Le Pokémon le plus fort en critique est Méga-Shuckle, avec 230 points de critique.","Le Pokémon le plus fort en critique évité est Méga-Shuckle, avec 230 points de critique évité.","Le Pokémon le plus fort en critique multiplié est Méga-Shuckle, avec 230 points de critique multiplié.","Le Pokémon le plus fort en critique évité multiplié est Méga-Shuckle, avec 230 points de critique évité multiplié.","Le Pokémon le plus fort en critique évité multiplié est Méga-Shuckle, avec 230 points de critique évité multiplié.","Le Pokémon le plus fort en critique évité multiplié est Méga-Shuckle, avec 230 points de critique évité multiplié.","Le Pokémon le plus fort"]
+let facts = ["Ikue Otani est la voix universelle de Pikachu","Au départ, Arcanin devait être un Pokémon légendaire","Tygnon et Kicklee sont inspirés de champions sportifs","Les 3 oiseaux légendaires de la 1ère génération portent des noms de dieux"]
 
 const displayPopup = (isDisplay) => {
     const popup = document.querySelector('.modal-popup');
@@ -25,10 +25,5 @@ buttonClosePopup.addEventListener('click', () => {
     displayPopup(false);
 });
 
-document.addEventListener("DOMContentLoaded", () => {
-    setTimeout(() => {
-        displayPopup(true);
-    }, 10000);
-});
 
 
